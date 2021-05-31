754c28ea353d51557e4b06833bddc38b0ef446c9
sezann <elvirakirshina@gmail.com>
2021-05-31 06:49:07 +0300

    Правки ESlint

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const randomInteger = function (min,max) {
   if (max > min && min >= 0 && max >0) {
-    return Math.floor (min + Math.random()*(max + 1 - min))
+    return Math.floor (min + Math.random()*(max + 1 - min));
   }
   throw new RangeError;
 };
