fb4edbd90e49180c5b60e134a65d88ad3a9925b0
sezann <elvirakirshina@gmail.com>
2021-05-31 06:44:37 +0300

    Вспомогательные функции

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const randomInteger = function (min,max) {
+  if (max > min && min >= 0 && max >0) {
+    return Math.floor (min + Math.random()*(max + 1 - min))
+  }
+  throw new RangeError;
+};
+
+const randomNonInteger = function (min, max, decimalNumber) {
+  if (max > min && min >= 0 && max >0) {
+    return (min + Math.random() * (max + 1 - min)).toFixed(decimalNumber);
+  }
+  throw new RangeError;
+};
