d19b19b74a30aee923baef6b67e34f13829fb9de
sezann <elvirakirshina@gmail.com>
2021-06-22 07:21:20 +0300

    Правки функции createCard

--- js/popup.js
+++ js/popup.js
@@ -5,11 +5,31 @@ const similarAdTemplateElement = document.querySelector('#card');
 const similarAdTemplate = similarAdTemplateElement.content.querySelector('.popup');
 const similarAds = CREATE_AD();
 
-// правильно ли я объявила функцию, ее параметры, и можно ли писать такую конструкцию сразу после return?
-
-function createCard (similarAds) {
-  return similarAds.forEach((offer) => {
+const createCard = (data) => {
     const adElement = similarAdTemplate.cloneNode(true);
+
+    const renderFeatures = (featureListElement, features) => {
+      const featureListElement = adElement.querySelector('.popup__features');
+      featureListElement.innerHTML = '';
+
+      features.forEach((item) => {
+        const li = document.createElement('li');
+        li.classList.add(item);
+        featureListElement.appendChild(li);
+      });
+    };
+
+    const renderPhotos = (photosListElement, photos) => {
+      const photosListElement = adElement.querySelector('.popup__photos');
+      photosListElement.innerHTML = '';
+
+      photos.forEach((url) => {
+        const img = document.createElement('img');
+        img.src = url;
+        photosListElement.appendChild(img);
+      });
+    };
+
     adElement.querySelector('.popup__avatar').src = author.avatar;
     adElement.querySelector('.popup__title').textContent = offer.title;
     adElement.querySelector('.popup__text--address').textContent = offer.address;
@@ -17,37 +37,15 @@ function createCard (similarAds) {
     adElement.querySelector('.popup__type').textContent = offer.type;
     adElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
     adElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
-    adElement.querySelector('.popup__features').textContent = offer.features;
     adElement.querySelector('.popup__description').textContent = offer.description;
-    adElement.querySelector('.popup__photos').src = offer.photos;
-    similarListFragment.appendChild(adElement);
-  });
-};
 
-// правильно ли передаю здесь параметры?
-// и правильно ли понимаю логику рендера?
+    return adElement;
+};
 
 const renderCard = (createCard) => {
     const similarListFragment = document.createDocumentFragment();
-    createCard();
-    notice.innerHTML = '';
-    notice.appendChild(similarListFragment);
+    notice.appendChild(similarListFragment, createCard(data));
 };
-
-// блок с преимуществами
-// не понимаю как вызвать функцию внутри создания карточки
-
-const featureListElement = document.querySelector('.popup__features');
-featureListElement.innerHTML = '';
-
-const renderFeatures = (featureListElement, features) => {
-  features.forEach((item) => {
-    const li = document.createElement('li');
-    li.classList.add(item);
-    featureListElement.appendChild(li);
-  });
-};
-
 
 export {renderCard};
 
