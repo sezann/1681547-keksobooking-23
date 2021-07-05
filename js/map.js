8aa157dd7a33955b5a251c1160561b8503a783b8
sezann <elvirakirshina@gmail.com>
2021-07-05 07:54:44 +0300

    исправила путь к иконкам

--- js/map.js
+++ js/map.js
@@ -28,7 +28,7 @@ const map = L.map('map-canvas')
 
 
 const mainPinIcon = L.icon({
-  iconUrl: '/img/main-pin.svg',
+  iconUrl: 'img/main-pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52],
 });
@@ -52,7 +52,7 @@ const createMarker = (point) => {
   const {lat, lng} = point;
 
   const icon = L.icon({
-    iconUrl: '/img/pin.svg',
+    iconUrl: 'img/pin.svg',
     iconSize: [40, 40],
     iconAnchor: [20, 40],
   });
